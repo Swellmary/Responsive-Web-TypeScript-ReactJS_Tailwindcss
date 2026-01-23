ce65dadb6b0bef3813fa30f915bb7efa690430c1
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-23 20:08:11 +0800

    add useState to open Menu | initial formula

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
 import { Menu } from "lucide-react";
+import { useState } from "react";
 
 export default function NavBar() {
+    
+    // useState: Menu is Open
+    const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
     return <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blue-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
@@ -23,11 +28,13 @@ export default function NavBar() {
                     <a href="#pricing" className="text-gray-300 hover:text-white text-sm lg:text-base">Pricing</a>
                     <a href="#testimonials" className="text-gray-300 hover:text-white text-sm lg:text-base">Testimonials</a>
                 </div>
-                
+
                 {/* Responsive Mobile NavBar - Humberger Icon*/}
-                <button className="md:hidden p-2 text-gray-300 hover:text-white">
+                <button className="md:hidden p-2 text-gray-300 hover:text-white" onClick={() => setMobileMenuIsOpen((prev) => !prev)}>
                     <Menu className="w-5 h-5 sm:w-6 sm:h-6"/>
                 </button>
+
+                {mobileMenuIsOpen && <p>Hello</p>}
             </div>
         </div>
     </nav>
