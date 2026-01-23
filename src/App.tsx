10b64e3643d0aebfb1d58aaaabd759396eb5b112
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-23 15:13:48 +0800

    import sections to App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Navbar  from "./components/Navbar"
+import Hero  from "./components/Hero"
+import Features  from "./components/Pricing"
+import Pricing  from "./components/Pricing"
+import Testimonials  from "./components/Testimonials"
+import Footer  from "./components/Footer"
+
 
 function App() {
 
   return <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
-    <Nav />
+    <Navbar />
     <Hero />
-    <Feaures />
+    <Features />
     <Pricing />
     <Testimonials />
     <Footer />
