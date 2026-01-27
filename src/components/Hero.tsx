b318909f5a42bf91371e881359b24d122cd9e6af
Well Mary Suarez <suarezwellmary@gmail.com>
2026-01-27 22:22:40 +0800

    add ChevronDown/arrow down on IDE header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { ChevronDown } from "lucide-react";
 
 export default function Hero() {
     // gives a glow where your mouse is poiting
@@ -38,8 +39,9 @@ export default function Hero() {
                                     <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500"/>
                                     <div className="w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500"/>
                                 </div>
-                                <span className="text-xs sm:text-m text-gray-300">CodeFlow AI</span>
+                                <span className="text-xs sm:text-sm text-gray-300">CodeFlow AI</span>
                             </div>
+                            <ChevronDown className="w-3 h-3 sm:w-4 sm:h-4 text-gray-400"/>
                         </div>
                     </div>
                 </div>
